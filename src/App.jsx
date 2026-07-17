0d5896b650a6a6192dc600e0743c0eb5e52557ba
sachinc555555-cell <sachinc555555-cell@users.noreply.github.com>
2026-07-17 23:01:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import MovieCard from "./jun-19/MovieCard";
+import { movieData } from "./jun-19/movie";
+import ClassCounter from "./jun-24/ClassCounter";
+import Click from "./jun-24/Click";
+import LikeButton from "./jun-24/LikeButton";
+import ClassClick from "./jun-24/ClassClick";
+import Fan from "./jun-24/Fan";
+import Chair from "./jun-24/Chair";
+import Routing from "./jun-26/Routing";
+import Count from "./jun-29/Count";
+import Basic from "./jun-29/Basic";
+import OnlyOnce from "./jun-29/OnlyOnce";
+import Dependency from "./jun-29/Dependency";
+import Wall from "./jun-29/Wall";
+import RestaurantCard from "./jul-6/condition/RestaurantCard";
+import GetApiData from "./jun-29/GetApiData";
+import ContextOne from "./jul-8/ContextOne";
+import Pure from "./jul-8/Pure";
+import MemoExample from "./jul-8/MemoExample";
+import PostForm from "./jul-13/PostForm";
+import CounterRedux from "./jul-13/CounterRedux";
+import MemeGenerator from "./jul-13/MemeGenerator";
+import Header from "./jul-13/Header";
+import Attendance from "./jul-13/Attendance";
+import Todo from "./jul-13/Todo";
+
+function App() {
+  return (
+    <div>
+      <Routing />
+    </div>
+  );
+}
+
+export default App;
